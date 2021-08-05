df59d19d65be1f9d451502fcc0bd996c504a7ecb
harshit0606 <harshitasr001@gmail.com>
2021-08-05 10:13:26 +0530

    sidebar part1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import Header from "./Header"
+import Header from "./Header";
+import Recommended from './Recommended';
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className="main_page">
+      <Sidebar/>
+      <Recommended/>
+      </div>
+      
     </div>
   );
 }
