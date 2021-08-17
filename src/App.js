1e1c43dfe7e2b962f53e3761c05e61e4293bdfbb
harshit0606 <harshitasr001@gmail.com>
2021-08-17 17:44:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import {useState} from "react";
 import './App.css';
 import Header from "./Header";
 import Recommended from './Recommended';
 import Sidebar from "./Sidebar";
+import HomePage from "./HomePage";
+
+
 
 function App() {
+  
+
   return (
     <div className="App">
       <Header/>
-      <div className="main_page">
-      <Sidebar/>
-      <Recommended/>
-      </div>
+      <HomePage/>
+      
       
     </div>
   );
@@ -19,3 +23,11 @@ function App() {
 export default App;
 
 //Lets start by making youtube navbar first
+
+
+//Hey everyone welcome to our new video 
+//In this video we'll be completing our youtube home page
+
+
+//We have completed our home page in the next video we'll make the search page
+//Till then good bye and dont forget to like and comment and if you are new then hit that subscibe button
--- src/components/SidebarRow.js
+++ src/components/SidebarRow.js
@@ -3,7 +3,7 @@ import "./SidebarRow.css"
 
 function SidebarRow(props) {
     return (
-        <div className="sidebar_row">
+        <div className={`sidebar_row ${props.selected && "selected"}`}>
             <props.icon className="sidebarRow_icon"/>
             <h2 className="sidebarRow_title">{props.title}</h2>
         </div>
